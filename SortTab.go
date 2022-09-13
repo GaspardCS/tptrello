de55fe6cd37945b6699cc81d7471d203137da338
root <root@LAPTOP-EG3KEJP7.localdomain>
2022-09-13 11:18:03 +0200

    SortTab.go

--- SortTab.go
+++ SortTab.go
@@ -1,1 +1,32 @@
-test
+package piscine
+
+import (
+	"sort"
+)
+
+func SortTab(tab []int) []int {
+	sort.Sort(sort.IntSlice(tab))
+	return tab
+}
+
+/*func SortTab(tab []int) []int {
+	i := 0
+	u := 0
+	var t []int
+	for _, r := range tab {
+		if r > i {
+			t = []int{r}
+			u++
+		} else {
+			t = []int{r}
+			u++
+		}
+	}
+	return t
+}*/
+
+/*func main() {
+	tab := []int{45, 12, 24, 4, 1}
+	tab = SortTab(tab)
+	fmt.Println(tab)
+} */
