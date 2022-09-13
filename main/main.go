ac7ad3d37c9743444fb5d5877165b14f1294da20
root <root@LAPTOP-EG3KEJP7.localdomain>
2022-09-13 11:55:11 +0200

    test

--- main/main.go
+++ main/main.go
@@ -1,15 +1,14 @@
 package main
 
 import (
+	"piscine"
+
 	"github.com/01-edu/z01"
 )
 
 func main() {
-    tab := []int{45, 12, 24, 4, 1}
-	tab = SortTab(tab)
-	tab = CalculateTab(tab)
-	tab = SquareTab(tab)
-	tab = PrimeTab(tab)
+	tab := []int{45, 12, 24, 4, 1}
+	tab = piscine.SortTab()
 	for _, k := range tab {
 		if k == 7 {
 			z01.PrintRune('7')
