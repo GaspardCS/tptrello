ec1685c29af6c0ba61a4351f609c09bbe55c140c
KTHOMAS <thomas.kauffmant@ynov.com>
2022-09-13 11:01:01 +0200

    function finish

--- squareTab.go
+++ squareTab.go
@@ -1,5 +1,19 @@
 package piscine
 
-func squareTab(tab) {
-	println("TEST")
+func Sqrt(x int) int {
+	var z int = 1
+	for i := 0; i < 1000; i++ {
+		z -= (z*z - x) / (2 * z)
+	}
+	return z
+}
+
+func SquareTab(tab []int) []int {
+	var secondTab = []int{}
+
+	for i := 0; i <= len(tab)-1; i++ {
+		secondTab = append(secondTab, Sqrt(tab[i]))
+	}
+
+	return secondTab
 }
